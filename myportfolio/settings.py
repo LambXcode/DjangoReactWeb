a2b3495206ed896946cc9e7624f585765c7aae0d
Michael <mlamb.info@gmail.com>
2023-07-31 16:32:58 +0200

    DB Issues

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # if os.path.isfile(dotenv_file):
 #     dotenv.load_dotenv(dotenv_file)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
 
 # DATABASES = {}
 # DATABASES['default'] = dj_database_url.config(conn_max_age=500)
